e9f154e642e27946721c49a6c3e0515777a0f52d
Elliott Etzkorn <mail@elliottetzkorn.com>
2026-06-15 14:08:28 -0400

    Adjust texture opacity

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -292,7 +292,7 @@ const STAR_PATH = 'M15.9302 8.49121H23.125L23.8843 10.7349L18.009 15.2209L20.261
   background-image: url("data:image/svg+xml,%3Csvg viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.8' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E");
   background-repeat: repeat;
   background-size: 200px 200px;
-  opacity: 0.15;
+  opacity: 0.18;
   mix-blend-mode: luminosity;
   pointer-events: none;
 }
